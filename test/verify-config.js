79943e76d17a0047dde4c4996df35f172890e3dc
TaniaPash <t.pashnyuk@gmail.com>
2018-04-05 12:51:40 +0300

    refactor tests

--- test/verify-config.js
+++ test/verify-config.js
@@ -1,21 +1,33 @@
 const test = require('ava');
 const plugin = require('..');
 
-test('check that normal dependencies are working', t => {
-	const res = plugin.verifyConditions({
+
+/* test('check that normal dependencies are working', t => {
+    return plugin.verifyConditions({
 		pkgPath: './test/helpers/passingpackage.json',
 		devDependencies: true,
 		dependencies: true,
 		regExps: []
-	});
-    console.log(res);
-	t.is(res, 'fulfilled');
+	}).then(() => {
+        t.pass();
+       })
+}); */
+
+test('check that normal dependencies are working', t => {
+    return plugin.verifyConditions({
+        pkgPath: './test/helpers/passingpackage.json',   // it fails but works with failingpackage.json 
+        devDependencies: true,
+        dependencies: true,
+        regExps: []
+    }).then(t.pass);;
 });
+
 
 /*
  Test('check that normal dependencies are working', function(){
          const error = t.throws(() => {
             plugin.verifyConditions({
+        regExps: []
                pkgPath: './test/data/failingpackage.json',
                devDependencies: true,
                dependencies: true,
