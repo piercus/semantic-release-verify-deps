7308ea7fee5fd66404329fde045a1862e92958a3
TaniaPash <t.pashnyuk@gmail.com>
2018-04-05 14:37:35 +0300

    add second test

--- test/verify-config.js
+++ test/verify-config.js
@@ -1,39 +1,35 @@
 const test = require('ava');
 const plugin = require('..');
 
-
-/* test('check that normal dependencies are working', t => {
-    return plugin.verifyConditions({
+test('check that normal dependencies are working', t => { // Xo: Prefer using async/await instead of returning a Promise
+	return plugin.verifyConditions({ // Xo: Unknown assertion method bind
 		pkgPath: './test/helpers/passingpackage.json',
 		devDependencies: true,
 		dependencies: true,
 		regExps: []
-	}).then(() => {
-        t.pass();
-       })
-}); */
-
-test('check that normal dependencies are working', t => {
-    return plugin.verifyConditions({
-        pkgPath: './test/helpers/passingpackage.json',   // it fails but works with failingpackage.json 
-        devDependencies: true,
-        dependencies: true,
-        regExps: []
-    }).then(t.pass);;
+	}).then(t.pass.bind(t));
 });
 
+test('rejects', async t => {
+	const error = await t.throws(
+        plugin.verifyConditions({
+        	pkgPath: './test/helpers/failingpackage.json',
+        	devDependencies: true,
+        	dependencies: true,
+        	regExps: ['[a-zA-Z]+$']
+        }));
+    // Console.log(error.message);
+    t.is(error.message, error.message);
+});
 
-/*
- Test('check that normal dependencies are working', function(){
-         const error = t.throws(() => {
-            plugin.verifyConditions({
-        regExps: []
-               pkgPath: './test/data/failingpackage.json',
-               devDependencies: true,
-               dependencies: true,
-               regExps: ["[a-zA-Z]+$"]
-            })
-     });
-
-     t.is(error.message);
- }); */
+/* Test('rejects', t => {
+    const error = t.throws(
+    plugin.verifyConditions({
+        pkgPath: './test/helpers/failingpackage.json',
+        devDependencies: true,
+        dependencies: true,
+        regExps: ["[a-zA-Z]+$"]
+    }))
+    console.log("g",error);
+    t.is(error, null);
+}); */
