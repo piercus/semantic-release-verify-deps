e0ae89083363648486f22fba948d7789ccf2f868
TaniaPash <t.pashnyuk@gmail.com>
2018-04-06 15:23:28 +0300

    xo fix

--- test/verify-config.js
+++ test/verify-config.js
@@ -2,23 +2,25 @@ const test = require('ava');
 const plugin = require('..');
 
 test('check that normal dependencies are working', t => {
-    return plugin.verifyConditions({
-        pkgPath: './test/helpers/passingpackage.json',
-        devDependencies: true,
-        dependencies: true,
-        regExps: []
-    }).then(t.pass(t));
+	return plugin.verifyConditions({
+		pkgPath: './test/helpers/passingpackage.json',
+		devDependencies: true,
+		dependencies: true,
+		regExps: []
+	}).then(t.pass(t));
 });
 
 test('check that invalid dependencies are working #1', async t => {
-    const error = await t.throws(plugin.verifyConditions(
-        { pkgPath: './test/helpers/failingpackage.json', devDependencies: true, dependencies: true, regExps: ['[a-zA-Z]+$'] }
+	const error = await t.throws(plugin.verifyConditions(
+        {pkgPath: './test/helpers/failingpackage.json', devDependencies: true, dependencies: true, regExps: ['[a-zA-Z]+$']}
     ));
-    t.truthy(error.message); //.fail([message]).pass([message])
+    t.truthy(error.message); // .fail([message]).pass([message])
 });
 
 test.failing('check that invalid dependencies are working #2', t => {
     t.throws(plugin.verifyConditions(
-        { pkgPath: './test/helpers/failingpackage.json', devDependencies: true, dependencies: true, regExps: ['[a-zA-Z]+$'] }
-    )).then(err => {t.truthy(err.message); });
+        {pkgPath: './test/helpers/failingpackage.json', devDependencies: true, dependencies: true, regExps: ['[a-zA-Z]+$']}
+    )).then(err => {
+t.truthy(err.message);
+    });
 });
