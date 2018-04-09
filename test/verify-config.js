3f7da11e7521978397fcef9142144e4e96ee3fb7
TaniaPash <t.pashnyuk@gmail.com>
2018-04-09 11:43:01 +0300

    refactor ava tests

--- test/verify-config.js
+++ test/verify-config.js
@@ -10,14 +10,7 @@ test('check that normal dependencies are working', t => {
 	}).then(t.pass(t));
 });
 
-test('check that invalid dependencies are working #1', async t => {
-	const error = await t.throws(plugin.verifyConditions(
-        {pkgPath: './test/helpers/failingpackage.json', devDependencies: true, dependencies: true, regExps: ['[a-zA-Z]+$']}
-    ));
-    t.truthy(error.message); // .fail([message]).pass([message])
-});
-
-test.failing('check that invalid dependencies are working #2', t => {
+test.failing('check that invalid dependencies are working', t => {
     t.throws(plugin.verifyConditions(
         {pkgPath: './test/helpers/failingpackage.json', devDependencies: true, dependencies: true, regExps: ['[a-zA-Z]+$']}
     )).then(err => {
